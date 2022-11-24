4b443fe5d6417d2ddb7299985bc167fa3b6c79ed
bonnah john <bonnahpoku@gmail.com>
2022-11-24 09:04:09 -0800

    hero img,students fans ,mobile nav has been change and build

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 window.addEventListener("scroll", () => {
   var navBar = document.querySelector("nav");
-
+  var header = document.querySelector("header");
+  header.classList.toggle("change", window.scrollY > 0);
   navBar.classList.toggle("fix", window.scrollY > 0);
 });
